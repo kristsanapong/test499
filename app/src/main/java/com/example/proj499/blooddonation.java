da6e362c2defce3e83363e40fa1c1f41c5be42e4
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2019-07-04 23:17:46 +0700

    Blood Donation reserve queue.

--- app/src/main/java/com/example/proj499/blooddonation.java
+++ app/src/main/java/com/example/proj499/blooddonation.java
@@ -7,7 +7,6 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.app.Activity;
 
 public class blooddonation extends AppCompatActivity {
 
