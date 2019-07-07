d8b9c7a9ef873eac4bba08d70b6d96e9cb3fb5e9
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2019-07-07 22:04:14 +0700

    applogo

--- app/src/main/java/com/example/proj499/ManageDataTechnician.java
+++ app/src/main/java/com/example/proj499/ManageDataTechnician.java
@@ -1,7 +1,9 @@
 package com.example.proj499;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ManageDataTechnician extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class ManageDataTechnician extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_data_technician);
+    }
+
+    public void ManageMedical(View view)
+    {
+        Intent intent = new Intent(getBaseContext(), MedicalActivity.class);
+        startActivity(intent);
+
+    }
+    public void ManageAdmin(View view)
+    {
+        Intent intent = new Intent(getBaseContext(), TechnicianActivity.class);
+        startActivity(intent);
+
     }
 }
