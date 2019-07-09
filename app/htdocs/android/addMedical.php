139c818342fa7ebe7e61f58fae6ccaef5bdcbf9e
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2019-07-09 15:44:58 +0700

    add medical data pHp

---
+++ app/htdocs/android/addMedical.php
@@ -0,0 +1,46 @@
+<?php 
+ 
+	/*
+	* Created by Belal Khan
+	* website: www.simplifiedcoding.net 
+	* Retrieve Data From MySQL Database in Android
+	*/
+	
+	//database constants
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'blood_donation');
+	
+	//connecting to database and getting the connection object
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	
+	//Checking if any error occured while connecting
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+	}
+	
+	//creating a query
+	$stmt = $conn->prepare("SELECT username, password, firstname, lastname FROM technician_account;");
+	
+	//executing the query 
+	$stmt->execute();
+	
+	//binding results to the query 
+	$stmt->bind_result($username, $password, $firstname, $lastname);
+	
+	$account = array(); 
+	
+	//traversing through all the result 
+	while($stmt->fetch()){
+		$temp = array();
+		$temp['username'] = $username; 
+		$temp['password'] = $password; 
+		$temp['firstname'] = $firstname; 
+		$temp['lastname'] = $lastname; 
+		array_push($account, $temp);
+	}
+	
+	//displaying the result in json format 
+	echo json_encode($account);
