9832bb45de3c3c7cc4df00be4a9c738ab73cd9e7
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2019-07-09 14:07:12 +0700

    Blood Donation Validate

--- app/src/main/java/com/example/proj499/blooddonation.java
+++ app/src/main/java/com/example/proj499/blooddonation.java
@@ -10,36 +10,59 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class blooddonation extends AppCompatActivity {
-
-//    int score=0;
-//    RadioGroup G_q1 = (RadioGroup)findViewById(R.id.q1);
-//    RadioGroup G_q2 = (RadioGroup)findViewById(R.id.q2);
-//    RadioGroup G_q3 = (RadioGroup)findViewById(R.id.q3);
-//    RadioGroup G_q4 = (RadioGroup)findViewById(R.id.q4);
-//    RadioGroup G_q5 = (RadioGroup)findViewById(R.id.q5);
-//    RadioGroup G_q6 = (RadioGroup)findViewById(R.id.q6);
-//    RadioGroup G_q7 = (RadioGroup)findViewById(R.id.q7);
-//    RadioGroup G_q8 = (RadioGroup)findViewById(R.id.q8);
-//    RadioGroup G_q9 = (RadioGroup)findViewById(R.id.q9);
-//    RadioGroup G_q10 = (RadioGroup)findViewById(R.id.q10);
-//    RadioGroup G_q11 = (RadioGroup)findViewById(R.id.q11);
-//    RadioGroup G_q12 = (RadioGroup)findViewById(R.id.q12);
-//    RadioGroup G_q13 = (RadioGroup)findViewById(R.id.q13);
-//    RadioGroup G_q14 = (RadioGroup)findViewById(R.id.q14);
-//    RadioGroup G_q15 = (RadioGroup)findViewById(R.id.q15);
-//    RadioGroup G_q16 = (RadioGroup)findViewById(R.id.q16);
-//    RadioGroup G_q17 = (RadioGroup)findViewById(R.id.q17);
-//    RadioGroup G_q18 = (RadioGroup)findViewById(R.id.q18);
-//    RadioGroup G_q19 = (RadioGroup)findViewById(R.id.q19);
-//    RadioGroup G_q20 = (RadioGroup)findViewById(R.id.q20);
-//    RadioGroup G_q21 = (RadioGroup)findViewById(R.id.q21);
-//    RadioGroup G_q22 = (RadioGroup)findViewById(R.id.q22);
-//    RadioGroup G_q23 = (RadioGroup)findViewById(R.id.q23);
-//    RadioGroup G_q24 = (RadioGroup)findViewById(R.id.q24);
-//    RadioGroup G_q25 = (RadioGroup)findViewById(R.id.q25);
-//    RadioGroup G_q26 = (RadioGroup)findViewById(R.id.q26);
-//    RadioGroup G_q27 = (RadioGroup)findViewById(R.id.q27);
-//    RadioGroup G_q28 = (RadioGroup)findViewById(R.id.q28);
+    private static final int RB1_ID = 1000;//first radio button id
+    private static final int RB2_ID = 1001;//second radio button id
+    int score = 0;
+    RadioGroup G_q1 = (RadioGroup) findViewById(R.id.q1);
+    RadioGroup G_q2 = (RadioGroup) findViewById(R.id.q2);
+    RadioGroup G_q3 = (RadioGroup) findViewById(R.id.q3);
+    RadioGroup G_q4 = (RadioGroup) findViewById(R.id.q4);
+    RadioGroup G_q5 = (RadioGroup) findViewById(R.id.q5);
+    RadioGroup G_q6 = (RadioGroup) findViewById(R.id.q6);
+    RadioGroup G_q7 = (RadioGroup) findViewById(R.id.q7);
+    RadioGroup G_q8 = (RadioGroup) findViewById(R.id.q8);
+    RadioGroup G_q9 = (RadioGroup) findViewById(R.id.q9);
+    RadioGroup G_q10 = (RadioGroup) findViewById(R.id.q10);
+    RadioGroup G_q11 = (RadioGroup) findViewById(R.id.q11);
+    RadioGroup G_q12 = (RadioGroup) findViewById(R.id.q12);
+    RadioGroup G_q13 = (RadioGroup) findViewById(R.id.q13);
+    RadioGroup G_q14 = (RadioGroup) findViewById(R.id.q14);
+    RadioGroup G_q15 = (RadioGroup) findViewById(R.id.q15);
+    RadioGroup G_q16 = (RadioGroup) findViewById(R.id.q16);
+    RadioGroup G_q17 = (RadioGroup) findViewById(R.id.q17);
+    RadioGroup G_q18 = (RadioGroup) findViewById(R.id.q18);
+    RadioGroup G_q19 = (RadioGroup) findViewById(R.id.q19);
+    RadioGroup G_q20 = (RadioGroup) findViewById(R.id.q20);
+    RadioGroup G_q21 = (RadioGroup) findViewById(R.id.q21);
+    RadioGroup G_q22 = (RadioGroup) findViewById(R.id.q22);
+    RadioGroup G_q23 = (RadioGroup) findViewById(R.id.q23);
+    RadioGroup G_q24 = (RadioGroup) findViewById(R.id.q24);
+    RadioGroup G_q25 = (RadioGroup) findViewById(R.id.q25);
+    int btn1 = G_q1.getCheckedRadioButtonId();
+    int btn2 = G_q2.getCheckedRadioButtonId();
+    int btn3 = G_q3.getCheckedRadioButtonId();
+    int btn4 = G_q4.getCheckedRadioButtonId();
+    int btn5 = G_q5.getCheckedRadioButtonId();
+    int btn6 = G_q6.getCheckedRadioButtonId();
+    int btn7 = G_q7.getCheckedRadioButtonId();
+    int btn8 = G_q8.getCheckedRadioButtonId();
+    int btn9 = G_q9.getCheckedRadioButtonId();
+    int btn10 = G_q10.getCheckedRadioButtonId();
+    int btn11 = G_q11.getCheckedRadioButtonId();
+    int btn12 = G_q12.getCheckedRadioButtonId();
+    int btn13 = G_q13.getCheckedRadioButtonId();
+    int btn14 = G_q14.getCheckedRadioButtonId();
+    int btn15 = G_q15.getCheckedRadioButtonId();
+    int btn16 = G_q16.getCheckedRadioButtonId();
+    int btn17 = G_q17.getCheckedRadioButtonId();
+    int btn18 = G_q18.getCheckedRadioButtonId();
+    int btn19 = G_q19.getCheckedRadioButtonId();
+    int btn20 = G_q20.getCheckedRadioButtonId();
+    int btn21 = G_q21.getCheckedRadioButtonId();
+    int btn22 = G_q22.getCheckedRadioButtonId();
+    int btn23 = G_q23.getCheckedRadioButtonId();
+    int btn24 = G_q24.getCheckedRadioButtonId();
+    int btn25 = G_q25.getCheckedRadioButtonId();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +71,204 @@ public class blooddonation extends AppCompatActivity {
 
     }
 
+
+    public void checkQuiz() {
+        switch (btn1) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn2) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn3) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn4) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn5) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn6) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn7) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn8) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn9) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn10) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn11) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn12) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn13) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn14) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn15) {
+            case RB1_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn16) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn17) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn18) {
+            case RB2_ID:
+                score++;
+                break;
+            case RB1_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn19) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn20) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn21) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn22) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn23) {
+            case RB1_ID:
+                score++;
+                break;
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn24) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+        switch (btn25) {
+            case RB2_ID:
+                score++;
+                break;
+            case -1:
+                break;
+        }
+
+
+        if (score == 25) {
+            //insert into queue
+            //show alertbox ยืนยันการจองคิว
+            //ใช่ ไม่
+
+        } else {
+            //alertbox ท่านไม่สามารถบริจาคโลหิตได้
+            //ok
+
+        }
+    }
+
+}
 
 
 //    public void checkDonation(){
@@ -130,5 +351,3 @@ public class blooddonation extends AppCompatActivity {
 //
 //
 //    }
-
-}
