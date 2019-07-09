cf3e4afb3239297f9c0417933af2460945737614
Kritsanapong Poomdokmai <kristsanapong_p@cmu.ac.th>
2019-07-09 16:04:06 +0700

    news and account php

---
+++ app/htdocs/android/news.php
@@ -0,0 +1,27 @@
+<?php
+	header("content-type:text/javascript;charset=utf-8");
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASS','');
+	define('DB','blood_donation');
+
+	if ($_SERVER['REQUEST_METHOD']=='GET') {
+
+		$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to connect');
+
+		mysqli_set_charset($con,"utf8");
+		$sql = "SELECT news.news_lead FROM `news`";
+		$r = mysqli_query($con,$sql);
+		$result = array();
+
+		while ($row = mysqli_fetch_array($r)){
+			$temp = array();
+			
+			$temp['news_lead'] = $row['news_lead'];
+			array_push($result, $temp);
+		}
+
+		echo json_encode(array('result' => $result));
+
+		mysqli_close($con);
+}
