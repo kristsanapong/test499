fb71ac2616349aa1f551cc68eea80e51bd22f381
wichayapoka <wichaya_poka@cmu.ac.th>
2019-07-11 00:10:19 +0700

    Show name in profile

--- app/htdocs/android/getHistory.php
+++ app/htdocs/android/getHistory.php
@@ -4,13 +4,15 @@ include 'db_connect.php';
 	if (isset($_POST)) {
 
 		$username = $_POST['username'];
-		$sql = "SELECT detail FROM history WHERE username = '$username'";
+		$sql = "SELECT detail, firstname, lastname FROM history WHERE username = '$username'";
 		$r = mysqli_query($con,$sql);
 		$result = array();
 
 		while ($row = mysqli_fetch_array($r)){
 			$temp = array();
 			$temp['detail'] = $row['detail'];
+			$temp['firstname'] = $row['firstname'];
+			$temp['lastname'] = $row['lastname'];
 			array_push($result, $temp);
 			// array_push($result, $row);
 		}
