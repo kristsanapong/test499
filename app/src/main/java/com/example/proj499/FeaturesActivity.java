8e8046d881e0f0fa9d543a220893a35fbf6d12b3
wichayapoka <wichaya_poka@cmu.ac.th>
2019-07-12 14:30:51 +0700

    Implement on back pressed

--- app/src/main/java/com/example/proj499/FeaturesActivity.java
+++ app/src/main/java/com/example/proj499/FeaturesActivity.java
@@ -1,9 +1,11 @@
 package com.example.proj499;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class FeaturesActivity extends AppCompatActivity {
 
@@ -54,5 +56,26 @@ public class FeaturesActivity extends AppCompatActivity {
         Intent intent = new Intent(getBaseContext(), MapsActivity.class);
         startActivity(intent);
 
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "กดปุ่ม Back อีกครั้งเพื่อออกจากระบบ", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
